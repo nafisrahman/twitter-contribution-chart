ae62c9977540ca4b2b9cd400138eadb4e6a21b04
nafisrahman <nafisrahman797@gmail.com>
2021-01-14 11:35:20 -0500

    first draft

--- src/App.js
+++ src/App.js
@@ -1,243 +1,252 @@
 /* @flow */
-import React, { Component } from 'react'
+import React from 'react'
 import User from './User'
-import FilePickComponent from './FilePickComponent'
-import ProgressBar from 'react-progress-bar-plus'
-import { drawContributions } from 'twitter-contributions-canvas'
-import { prepareToCanvasData } from './processData'
-import { parseData } from './parseData'
-import { type ContributionsByYear, type CanvasData } from './types'
+// import FilePickComponent from './FilePickComponent'
+// import ProgressBar from 'react-progress-bar-plus'
+// import { drawContributions } from 'twitter-contributions-canvas'
+// import { prepareToCanvasData } from './processData'
+// import { parseData } from './parseData'
+// import { type ContributionsByYear, type CanvasData } from './types'
 import './App.css'
 import './entireframework.min.css'
-import 'react-progress-bar-plus/lib/progress-bar.css'
+// import 'react-progress-bar-plus/lib/progress-bar.css'
 
-type State = {
-  error: ?string,
-  progress: number,
-  theme: string,
-  username: string,
-  rawTweetsFile: ?File,
-  parsedData: ?ContributionsByYear,
-  previewStats: ?string,
-}
+// type State = {
+//   error: ?string,
+//   progress: number,
+//   theme: string,
+//   username: string,
+//   rawTweetsFile: ?File,
+//   parsedData: ?ContributionsByYear,
+//   previewStats: ?string,
+// }
 
-class App extends Component<{}, State> {
-  availableThemes = {
-    standard: 'GitHub',
-    halloween: 'Halloween',
-    teal: 'Teal',
-    leftPad: '@left_pad',
-    dracula: 'Dracula',
-    blue: 'Blue',
-    panda: 'Panda 🐼',
-    sunny: 'Sunny',
-  }
-  state = {
-    error: null,
-    progress: -1,
-    theme: 'standard',
-    username: '',
-    rawTweetsFile: null,
-    parsedData: null,
-    previewStats: null,
-  }
+// class App extends React.Component<{}, State> {
+//   availableThemes = {
+//     standard: 'GitHub',
+//     halloween: 'Halloween',
+//     teal: 'Teal',
+//     leftPad: '@left_pad',
+//     dracula: 'Dracula',
+//     blue: 'Blue',
+//     panda: 'Panda 🐼',
+//     sunny: 'Sunny',
+//   }
+//   state = {
+//     error: null,
+//     progress: -1,
+//     theme: 'standard',
+//     username: '',
+//     rawTweetsFile: null,
+//     parsedData: null,
+//     previewStats: null,
+//   }
 
-  canvas = React.createRef()
-  handleUsernameChange = (e) => {
-    this.setState({
-      username: e.target.value,
-    })
-  }
-  handleSubmit = (e) => {
-    e.preventDefault()
-    if (!this.state.rawTweetsFile) {
-      this.setState({ error: 'Upload file to begin' })
-      return
-    }
-    this.setState({ progress: 1, error: null, parsedData: null }, () => {
-      setTimeout(
-        () =>
-          parseData(
-            this.state.rawTweetsFile,
-            (progress, resume) => {
-              this.setState({ progress }, resume)
-            },
-            (parsedTweetsNumber, parsedData) => {
-              console.log('Malformed tweets', parsedData.errors)
-              this.setState(
-                {
-                  progress: -1,
-                  parsedData,
-                  previewStats: `${parsedTweetsNumber} tweets parsed, ${parsedData.errors.length} malformed`,
-                },
-                () => this.drawCanvas()
-              )
-            },
-            (err) => this.setState({ error: 'Error while parsing csv' })
-          ),
-        300
-      )
-    })
-  }
-  onAcceptedFiles = (acceptedFiles: Array<File>) => {
-    const rawTweetsFile = acceptedFiles.find(
-      (file) => file.name.indexOf('tweets.csv') > -1
-    )
-    if (!rawTweetsFile) {
-      this.setState({ error: "Can't find tweets.csv" })
-    } else {
-      this.setState({ rawTweetsFile })
-    }
-  }
+//   canvas = React.createRef()
+//   handleUsernameChange = (e) => {
+//     this.setState({
+//       username: e.target.value,
+//     })
+//   }
 
-  drawCanvas() {
-    if (this.state.parsedData) {
-      const canvasData: CanvasData = prepareToCanvasData(this.state.parsedData)
-      drawContributions(this.canvas.current, {
-        data: canvasData,
-        username: this.state.username,
-        themeName: this.state.theme,
-        footerText: 'Made by @sallar for GitHub, converted by @ptmt',
-      })
-    } else {
-      this.setState({ error: 'Data is not parsed' })
-    }
-  }
-  downloadCanvas() {
-    try {
-      const dataUrl = this.canvas.current.toDataURL()
-      const a = document.createElement('a')
-      document.body.insertAdjacentElement('beforeend', a)
-      a.download = 'contributions.png'
-      a.href = dataUrl
-      a.click()
-      document.body.removeChild(a)
-    } catch (err) {
-      console.error(err)
-    }
-  }
-  render() {
-    return (
-      <React.Fragment>
-        <ProgressBar percent={this.state.progress} onTop />,
-        <FilePickComponent onFiles={this.onAcceptedFiles} disableClick>
-          <div className='wrapper'>
-            <div className='container'>
-              <div className='hero'>
-                <h1>Twitter Contribution Chart Generator</h1>
+//   handleSubmit = (e) => {
+//     e.preventDefault()
+//     if (!this.state.rawTweetsFile) {
+//       this.setState({ error: 'Upload file to begin' })
+//       return
+//     }
+//     this.setState({ progress: 1, error: null, parsedData: null }, () => {
+//       setTimeout(
+//         () =>
+//           parseData(
+//             this.state.rawTweetsFile,
+//             (progress, resume) => {
+//               this.setState({ progress }, resume)
+//             },
+//             (parsedTweetsNumber, parsedData) => {
+//               console.log('Malformed tweets', parsedData.errors)
+//               this.setState(
+//                 {
+//                   progress: -1,
+//                   parsedData,
+//                   previewStats: `${parsedTweetsNumber} tweets parsed, ${parsedData.errors.length} malformed`,
+//                 },
+//                 () => this.drawCanvas()
+//               )
+//             },
+//             (err) => this.setState({ error: 'Error while parsing csv' })
+//           ),
+//         300
+//       )
+//     })
+//   }
+//   onAcceptedFiles = (acceptedFiles: Array<File>) => {
+//     const rawTweetsFile = acceptedFiles.find(
+//       (file) => file.name.indexOf('tweets.csv') > -1
+//     )
+//     if (!rawTweetsFile) {
+//       this.setState({ error: "Can't find tweets.csv" })
+//     } else {
+//       this.setState({ rawTweetsFile })
+//     }
+//   }
 
-                <div className='msg'>
-                  <strong>Privacy disclaimer: </strong> All data is processed
-                  locally within your browser, no cookies or 3rd party server,
-                  nothing is stored or sent to the server. Check the source code
-                  on{' '}
-                  <a href='https://github.com/ptmt/twitter-contribution-chart'>
-                    GitHub
-                  </a>{' '}
-                  or run your own local copy.
-                </div>
+//   drawCanvas() {
+//     if (this.state.parsedData) {
+//       const canvasData: CanvasData = prepareToCanvasData(this.state.parsedData)
+//       drawContributions(this.canvas.current, {
+//         data: canvasData,
+//         username: this.state.username,
+//         themeName: this.state.theme,
+//         footerText: 'Made by @sallar for GitHub, converted by @ptmt',
+//       })
+//     } else {
+//       this.setState({ error: 'Data is not parsed' })
+//     }
+//   }
+//   downloadCanvas() {
+//     try {
+//       const dataUrl = this.canvas.current.toDataURL()
+//       const a = document.createElement('a')
+//       document.body.insertAdjacentElement('beforeend', a)
+//       a.download = 'contributions.png'
+//       a.href = dataUrl
+//       a.click()
+//       document.body.removeChild(a)
+//     } catch (err) {
+//       console.error(err)
+//     }
+//   }
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <ProgressBar percent={this.state.progress} onTop />,
+//         <FilePickComponent onFiles={this.onAcceptedFiles} disableClick>
+//           <div className='wrapper'>
+//             <div className='container'>
+//               <div className='hero'>
+//                 <h1>Twitter Contribution Chart Generator</h1>
 
-                <p>
-                  Request Twitter Archive from your profile here{' '}
-                  <a href='https://twitter.com/settings/account'>
-                    https://twitter.com/settings/account
-                  </a>
-                  , wait for an email from Twitter with .zip file attached,
-                  extract tweets.csv and dran'd'drop it to this page.
-                </p>
+//                 <div className='msg'>
+//                   <strong>Privacy disclaimer: </strong> All data is processed
+//                   locally within your browser, no cookies or 3rd party server,
+//                   nothing is stored or sent to the server. Check the source code
+//                   on{' '}
+//                   <a href='https://github.com/ptmt/twitter-contribution-chart'>
+//                     GitHub
+//                   </a>{' '}
+//                   or run your own local copy.
+//                 </div>
 
-                <div style={{ margin: '1em 0' }}>
-                  <FilePickComponent onFiles={this.onAcceptedFiles}>
-                    <button className='btn btn-a'>Open tweets.csv</button>
-                    {this.state.rawTweetsFile && (
-                      <p>
-                        Found {this.state.rawTweetsFile.name}, ready to proceed
-                      </p>
-                    )}
-                  </FilePickComponent>
-                </div>
+//                 <p>
+//                   Request Twitter Archive from your profile here{' '}
+//                   <a href='https://twitter.com/settings/account'>
+//                     https://twitter.com/settings/account
+//                   </a>
+//                   , wait for an email from Twitter with .zip file attached,
+//                   extract tweets.csv and dran'd'drop it to this page.
+//                 </p>
 
-                {this._renderForm()}
-              </div>
+//                 <div style={{ margin: '1em 0' }}>
+//                   <FilePickComponent onFiles={this.onAcceptedFiles}>
+//                     <button className='btn btn-a'>Open tweets.csv</button>
+//                     {this.state.rawTweetsFile && (
+//                       <p>
+//                         Found {this.state.rawTweetsFile.name}, ready to proceed
+//                       </p>
+//                     )}
+//                   </FilePickComponent>
+//                 </div>
 
-              {this.state.error && (
-                <div className='error'>{this.state.error}</div>
-              )}
+//                 {this._renderForm()}
+//               </div>
 
-              {this.state.parsedData && (
-                <div className='Results'>
-                  <h3>{this.state.previewStats}</h3>
-                  <div className='panel'>
-                    <label>
-                      Theme:
-                      <select
-                        value={this.state.theme}
-                        onChange={this._handleThemeChange}
-                      >
-                        {Object.keys(this.availableThemes).map((theme) => (
-                          <option key={theme} value={theme}>
-                            {this.availableThemes[theme]}
-                          </option>
-                        ))}
-                      </select>
-                    </label>
-                    <button
-                      className='smallbtn'
-                      onClick={() => this.downloadCanvas()}
-                      type='button'
-                    >
-                      Download the Image
-                    </button>
-                  </div>
+//               {this.state.error && (
+//                 <div className='error'>{this.state.error}</div>
+//               )}
 
-                  <canvas ref={this.canvas} />
-                </div>
-              )}
-            </div>
-          </div>
-        </FilePickComponent>
-        <User />
-      </React.Fragment>
-    )
-  }
+//               {this.state.parsedData && (
+//                 <div className='Results'>
+//                   <h3>{this.state.previewStats}</h3>
+//                   <div className='panel'>
+//                     <label>
+//                       Theme:
+//                       <select
+//                         value={this.state.theme}
+//                         onChange={this._handleThemeChange}
+//                       >
+//                         {Object.keys(this.availableThemes).map((theme) => (
+//                           <option key={theme} value={theme}>
+//                             {this.availableThemes[theme]}
+//                           </option>
+//                         ))}
+//                       </select>
+//                     </label>
+//                     <button
+//                       className='smallbtn'
+//                       onClick={() => this.downloadCanvas()}
+//                       type='button'
+//                     >
+//                       Download the Image
+//                     </button>
+//                   </div>
 
-  _handleThemeChange = (e) => {
-    this.setState({ theme: e.target.value }, () => this.drawCanvas())
-  }
+//                   <canvas ref={this.canvas} />
+//                 </div>
+//               )}
+//             </div>
+//           </div>
+//         </FilePickComponent>
+//         <User />
+//       </React.Fragment>
+//     )
+//   }
 
-  _renderForm = () => {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        {this.state.rawTweetsFile && (
-          <div>
-            <span className='addon'>@</span>
-            <input
-              ref={(ref) => {
-                this.inputRef = ref
-              }}
-              className='smooth'
-              placeholder='Your Twitter'
-              onChange={this.handleUsernameChange}
-              value={this.state.username}
-              id='username'
-            />
-          </div>
-        )}
+//   _handleThemeChange = (e) => {
+//     this.setState({ theme: e.target.value }, () => this.drawCanvas())
+//   }
 
-        {this.state.username.length > 0 && this.state.rawTweetsFile && (
-          <button
-            type='submit'
-            className='btn btn-b'
-            disabled={
-              this.state.username.length <= 0 || !this.state.rawTweetsFile
-            }
-          >
-            Generate!
-          </button>
-        )}
-      </form>
-    )
-  }
+//   _renderForm = () => {
+//     return (
+//       <form onSubmit={this.handleSubmit}>
+//         {this.state.rawTweetsFile && (
+//           <div>
+//             <span className='addon'>@</span>
+//             <input
+//               ref={(ref) => {
+//                 this.inputRef = ref
+//               }}
+//               className='smooth'
+//               placeholder='Your Twitter'
+//               onChange={this.handleUsernameChange}
+//               value={this.state.username}
+//               id='username'
+//             />
+//           </div>
+//         )}
+
+//         {this.state.username.length > 0 && this.state.rawTweetsFile && (
+//           <button
+//             type='submit'
+//             className='btn btn-b'
+//             disabled={
+//               this.state.username.length <= 0 || !this.state.rawTweetsFile
+//             }
+//           >
+//             Generate!
+//           </button>
+//         )}
+//       </form>
+//     )
+//   }
+// }
+
+function App() {
+  return (
+    <div>
+      <User />
+    </div>
+  )
 }
 
 export default App
--- src/processData.js
+++ src/processData.js
@@ -5,34 +5,34 @@ import {
   type ContributionsByYear,
   type CanvasData,
   type TweetRawLines,
-  type Year
-} from "./types";
+  type Year,
+} from './types'
 
 // https://github.com/sallar/github-contributions-api/blob/master/src/fetch.js
 const COLOR_MAP = [
-  "#196127",
-  "#239a3b",
-  "#7bc96f",
-  "#c6e48b",
-  "#ebedf0"
-].reverse();
+  '#196127',
+  '#239a3b',
+  '#7bc96f',
+  '#c6e48b',
+  '#ebedf0',
+].reverse()
 
 // TODO: proper normal distribution as github supposedly does
 function calculateIntensity(maxInYear: number, count: number): number {
   if (count === 0) {
-    return 0;
+    return 0
   }
-  const quartile = maxInYear > 4 ? maxInYear / 4 : 1;
+  const quartile = maxInYear > 4 ? maxInYear / 4 : 1
   if (count > 0 && count <= quartile) {
-    return 1;
+    return 1
   }
   if (count > quartile && count <= quartile * 2) {
-    return 2;
+    return 2
   }
   if (count > quartile && count <= quartile * 3) {
-    return 3;
+    return 3
   }
-  return 4;
+  return 4
 }
 
 export function reduceTweets(
@@ -40,99 +40,99 @@ export function reduceTweets(
   tweetsToProcess: Array<TweetRawLines>
 ): ContributionsByYear {
   const originalTweets = tweetsToProcess.filter(
-    f => !f.retweeted_status_id && !f.in_reply_to_status_id && f.timestamp
-  );
+    (f) => !f.retweeted_status_id && !f.in_reply_to_status_id && f.timestamp
+  )
   return originalTweets.reduce((acc, tweet) => {
-    const date = new Date(tweet.timestamp);
-    const year = date.getFullYear().toString();
+    const date = new Date(tweet.timestamp)
+    const year = date.getFullYear().toString()
     if (
       !tweet.timestamp ||
-      date.toString() === "Invalid Date" ||
+      date.toString() === 'Invalid Date' ||
       year.length > 4
     ) {
       return {
         ...acc,
-        errors: acc.errors.concat(tweet)
-      };
+        errors: acc.errors.concat(tweet),
+      }
     }
-    const day = iso(date);
+    const day = iso(date)
     return {
       years: {
         ...acc.years,
-        [year]: incrementYear(year, acc.years[year])
+        [year]: incrementYear(year, acc.years[year]),
       },
       days: {
         ...acc.days,
-        [day]: incrementDay(date, acc.days[day] ? acc.days[day] : null)
+        [day]: incrementDay(date, acc.days[day] ? acc.days[day] : null),
       },
-      errors: acc.errors
-    };
-  }, data);
+      errors: acc.errors,
+    }
+  }, data)
 }
 
 export function prepareToCanvasData(data: ContributionsByYear): CanvasData {
   const daysArray = Object.keys(data.days)
-    .map(k => data.days[k])
-    .sort((a, b) => a.timestamp - b.timestamp);
-  const minDate = new Date(daysArray[0].timestamp);
-  const maxDate = new Date(daysArray[daysArray.length - 1].timestamp);
+    .map((k) => data.days[k])
+    .sort((a, b) => a.timestamp - b.timestamp)
+  const minDate = new Date(daysArray[0].timestamp)
+  const maxDate = new Date(daysArray[daysArray.length - 1].timestamp)
   const maxContributionsByYear = daysArray.reduce((acc, day) => {
-    const year = new Date(day.timestamp).getFullYear();
+    const year = new Date(day.timestamp).getFullYear()
     return {
       ...acc,
-      [year]: day.count > (acc[year] || 0) ? day.count : acc[year]
-    };
-  }, {});
+      [year]: day.count > (acc[year] || 0) ? day.count : acc[year],
+    }
+  }, {})
 
   return {
     years: Object.keys(data.years)
-      .sort((a, b) => parseInt(b) - parseInt(a))
-      .map(k => ({
+      .sort((a, b) => Number(b) - Number(a))
+      .map((k) => ({
         ...data.years[k],
         range: {
           start:
             minDate.getFullYear() === k
               ? iso(minDate)
-              : iso(new Date(parseInt(k), 0, 1)),
+              : iso(new Date(Number(k), 0, 1)),
           end:
             maxDate.getFullYear() === k
               ? iso(maxDate)
-              : iso(new Date(parseInt(k), 11, 31))
-        }
+              : iso(new Date(Number(k), 11, 31)),
+        },
       })),
-    contributions: daysArray.map(day => {
+    contributions: daysArray.map((day) => {
       const intensity = calculateIntensity(
         maxContributionsByYear[
           new Date(day.timestamp).getFullYear().toString()
         ],
         day.count
-      );
+      )
 
       return {
         ...day,
         intensity,
-        color: COLOR_MAP[intensity]
-      };
-    })
-  };
+        color: COLOR_MAP[intensity],
+      }
+    }),
+  }
 }
 
 function incrementDay(date: Date, previous: ?DayContribution) {
-  var p = previous || { count: 0, date: iso(date), timestamp: date.getTime() };
+  var p = previous || { count: 0, date: iso(date), timestamp: date.getTime() }
   return {
     ...p,
-    count: p.count + 1
-  };
+    count: p.count + 1,
+  }
 }
 
 function incrementYear(year: string, previous: ?Year): Year {
-  var y = previous || { total: 0, year };
+  var y = previous || { total: 0, year }
   return {
     ...y,
-    total: y.total + 1
-  };
+    total: y.total + 1,
+  }
 }
 
 function iso(date: Date): string {
-  return date.toISOString().split("T")[0];
+  return date.toISOString().split('T')[0]
 }
